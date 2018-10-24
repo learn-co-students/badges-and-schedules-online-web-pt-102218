935cd598e3de50cf26d8a716de3c87562ecb7d2c
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-10-24 10:55:53 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(array)
+  array_2 = Array.new
+  num = 1
+  array.each do |array|
+    new = ("Hello, my name is #{array}.")
+    num += 1
+    return new
+    array_2.push(new)
+    array_2
+  end
+  array_2
+end
