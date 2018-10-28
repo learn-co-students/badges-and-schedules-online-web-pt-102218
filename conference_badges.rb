bc78036e96aa7f19021c36fea3b750ac63d11bd3
Evan Sturman <evan.sturman@gmail.com>
2018-10-28 03:44:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,10 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+def batch_badge_creator(attendees)
+  sentences = []
+  attendees.each do |attendee|
+    sentences << "Hello, my name is #{attendee}."
+  end
+  sentences
+end
